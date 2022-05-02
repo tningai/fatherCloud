c946cf05ab3eb2bedc336e814c456132f7c9a0d2
ningcb <ningaijob@sina.com>
2022-05-02 16:07:41 +0800

    hot-fix second commit

--- cloud-payment8003/src/main/java/com/cloud/payment/controller/PaymentController.java
+++ cloud-payment8003/src/main/java/com/cloud/payment/controller/PaymentController.java
@@ -71,6 +71,7 @@ public class PaymentController {
         System.out.println("hello git first edit");
         System.out.println("hello again git second edit");
         System.out.println("hello hot-fix for the first edit");
+        System.out.println("hot-fix edit for conflict");
         return serverPot;
     }
 }
